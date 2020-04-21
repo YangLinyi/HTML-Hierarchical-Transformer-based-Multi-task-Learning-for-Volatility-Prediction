340bb0137dd187c33b3ef088aedf3399773a86db
YangLinyi <yanglinyiucd@gmail.com>
2020-04-21 16:14:41 +0100

    Add files via upload

---
+++ Model/Sentence-Level-Transformer/transformers/transformers.py
@@ -0,0 +1,232 @@
+import os
+import time
+import matplotlib
+from tqdm import tqdm_notebook
+from datetime import date
+from matplotlib import pyplot as plt
+from pylab import rcParams
+
+#math package
+import random, math
+from numpy.random import seed
+from tensorflow import set_random_seed
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error
+
+#Pytorch Package
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+#Customized Transformers Util
+from transformer import util
+from transformer.util import mask_
+
+
+class SelfAttention(nn.Module):
+    def __init__(self, emb, heads=8, mask=False):
+        """
+        :param emb:
+        :param heads:
+        :param mask:
+        """
+
+        super().__init__()
+
+        self.emb = emb
+        self.heads = heads
+        self.mask = mask
+
+        self.tokeys = nn.Linear(emb, emb * heads, bias=False)
+        self.toqueries = nn.Linear(emb, emb * heads, bias=False)
+        self.tovalues = nn.Linear(emb, emb * heads, bias=False)
+
+        self.unifyheads = nn.Linear(heads * emb, emb)
+
+    def forward(self, x):
+
+        b, t, e = x.size()
+        h = self.heads
+        assert e == self.emb
+
+        keys    = self.tokeys(x)   .view(b, t, h, e)
+        queries = self.toqueries(x).view(b, t, h, e)
+        values  = self.tovalues(x) .view(b, t, h, e)
+
+        # compute scaled dot-product self-attention
+
+        # - fold heads into the batch dimension
+        keys = keys.transpose(1, 2).contiguous().view(b * h, t, e)
+        queries = queries.transpose(1, 2).contiguous().view(b * h, t, e)
+        values = values.transpose(1, 2).contiguous().view(b * h, t, e)
+
+        queries = queries / (e ** (1/4))
+        keys    = keys / (e ** (1/4))
+        # - Instead of dividing the dot products by sqrt(e), we scale the keys and values.
+        #   This should be more memory efficient
+
+        # - get dot product of queries and keys, and scale
+        dot = torch.bmm(queries, keys.transpose(1, 2))
+
+        assert dot.size() == (b*h, t, t)
+
+        if self.mask: # mask out the lower half of the dot matrix,including the diagonal
+            mask_(dot, maskval=float('-inf'), mask_diagonal=False)
+
+        dot = F.softmax(dot, dim=2) # dot now has row-wise self-attention probabilities
+
+        assert not util.contains_nan(dot[:, 1:, :]) # only the forst row may contain nan
+
+        if self.mask == 'first':
+            dot = dot.clone()
+            dot[:, :1, :] = 0.0
+            # - The first row of the first attention matrix is entirely masked out, so the softmax operation results
+            #   in a division by zero. We set this row to zero by hand to get rid of the NaNs
+
+        # apply the self attention to the values
+        out = torch.bmm(dot, values).view(b, h, t, e)
+
+        # swap h, t back, unify heads
+        out = out.transpose(1, 2).contiguous().view(b, t, h * e)
+
+        return self.unifyheads(out)
+
+class TransformerBlock(nn.Module):
+    def __init__(self, emb, heads, mask, seq_length, ff_hidden_mult=4, dropout=0.0):
+        super().__init__()
+
+        self.attention = SelfAttention(emb, heads=heads, mask=mask)
+        self.mask = mask
+
+        self.norm1 = nn.LayerNorm(emb)
+        self.norm2 = nn.LayerNorm(emb)
+
+        self.ff = nn.Sequential(
+            nn.Linear(emb, ff_hidden_mult * emb),
+            nn.ReLU(),
+            nn.Linear(ff_hidden_mult * emb, emb)
+        )
+
+        self.do = nn.Dropout(dropout)
+
+    def forward(self, x):
+
+        attended = self.attention(x)
+
+        x = self.norm1(attended + x)
+
+        x = self.do(x)
+
+        fedforward = self.ff(x)
+
+        x = self.norm2(fedforward + x)
+
+        x = self.do(x)
+
+        return x
+    
+##RTransformer
+
+class RTransformer(nn.Module):
+    """
+    Transformer for sequences Regression    
+    
+    """
+
+    def __init__(self, emb, heads, depth, seq_length, num_tokens, num_classes, max_pool=True, dropout=0.0):
+        """
+        :param emb: Embedding dimension
+        :param heads: nr. of attention heads
+        :param depth: Number of transformer blocks
+        :param seq_length: Expected maximum sequence length
+        :param num_tokens: Number of tokens (usually words) in the vocabulary
+        :param num_classes: Number of classes.
+        :param max_pool: If true, use global max pooling in the last layer. If false, use global
+                         average pooling.
+        """
+        super().__init__()
+
+        self.num_tokens, self.max_pool = num_tokens, max_pool
+        self.pos_embedding = nn.Embedding(embedding_dim=emb, num_embeddings=seq_length)
+
+        tblocks = []
+        for i in range(depth):
+            tblocks.append(
+                TransformerBlock(emb=emb, heads=heads, seq_length=seq_length, mask=False, dropout=dropout))
+
+        self.tblocks = nn.Sequential(*tblocks)
+
+        self.toprobs = nn.Linear(emb, num_classes)
+        self.toprobs_b = nn.Linear(emb, num_classes)
+        self.do = nn.Dropout(dropout)
+
+    def forward(self, x):
+        """
+        :param x: A batch by sequence length integer tensor of token indices.
+        :return: predicted log-probability vectors for each token based on the preceding tokens.
+        """
+        sentences_emb = x
+        b, t, e = x.size()
+
+        positions = self.pos_embedding(torch.arange(t))[None, :, :].expand(b, t, e)
+        
+        x = sentences_emb + positions
+        x = self.do(x)
+
+        x = self.tblocks(x)
+
+        x = x.max(dim=1)[0] if self.max_pool else x.mean(dim=1) # pool over the time dimension
+        
+        
+        x_a = self.toprobs(x)
+        x_b = self.toprobs_b(x)
+        x_a = torch.squeeze(x_a)
+        x_b = torch.squeeze(x_b)
+        
+        return x_a, x_b
+
+class CTransformer(nn.Module):
+    """
+    Transformer for classifying sequences
+    """
+
+    def __init__(self, emb, heads, depth, seq_length, num_tokens, num_classes, max_pool=True, dropout=0.0, wide=False):
+
+        super().__init__()
+
+        self.num_tokens, self.max_pool = num_tokens, max_pool
+
+        self.token_embedding = nn.Embedding(embedding_dim=emb, num_embeddings=num_tokens)
+        self.pos_embedding = nn.Embedding(embedding_dim=emb, num_embeddings=seq_length)
+
+        tblocks = []
+        for i in range(depth):
+            tblocks.append(
+                TransformerBlock(emb=emb, heads=heads, seq_length=seq_length, mask=False, dropout=dropout, wide=wide))
+
+        self.tblocks = nn.Sequential(*tblocks)
+
+        self.toprobs = nn.Linear(emb, num_classes)
+
+        self.do = nn.Dropout(dropout)
+
+    def forward(self, x):
+        """
+        :param x: A batch by sequence length integer tensor of token indices.
+        :return: predicted log-probability vectors for each token based on the preceding tokens.
+        """
+        tokens = self.token_embedding(x)
+        b, t, e = tokens.size()
+
+        positions = self.pos_embedding(torch.arange(t, device=d()))[None, :, :].expand(b, t, e)
+        x = tokens + positions
+        x = self.do(x)
+
+        x = self.tblocks(x)
+
+        x = x.max(dim=1)[0] if self.max_pool else x.mean(dim=1) # pool over the time dimension
+
+        x = self.toprobs(x)
+
+        return F.log_softmax(x, dim=1)
